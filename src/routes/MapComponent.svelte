5fbc616ded0fe83e6f2b96f63466664ace25a498
yama-kei <yamazaki@mierune.co.jp>
2024-10-21 10:18:07 +0900

    aadd marker

--- src/routes/MapComponent.svelte
+++ src/routes/MapComponent.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { MapLibre, GeoJSON, LineLayer } from 'svelte-maplibre';
+	import { MapLibre, GeoJSON, LineLayer, SymbolLayer, MarkerLayer } from 'svelte-maplibre';
+	let clickedFeature: { name?: string } | null = null;
 </script>
 
 <div class="h-full border-gray-600">
@@ -9,7 +10,9 @@
 		standardControls
 		center={[138.915, 36.383]}
 		zoom={10}
+		images={[{ id: 'busStopIcon', url: 'busStop.png' }]}
 	>
+		<!-- バスルート -->
 		<GeoJSON id="busRoute" data="data/bus_route.geojson">
 			<LineLayer
 				paint={{
@@ -19,5 +22,25 @@
 				}}
 			/>
 		</GeoJSON>
+
+		<!-- パス停 -->
+		<GeoJSON id="busStop" data="data/stops.geojson">
+			<SymbolLayer
+				layout={{
+					'icon-image': 'busStopIcon', // 事前に画像を定義して使用
+					'icon-size': 0.5,
+					'icon-allow-overlap': true, // アイコンが重なっても表示
+					'text-field': '{name}', // バス停の名前を表示
+					'text-offset': [0, -2], // アイコンとテキストの間隔
+					'text-size': 10 // テキストのサイズ
+				}}
+				minzoom={12}
+				on:click={(e) => (clickedFeature = e.detail.features?.[0]?.properties)}
+			/>
+		</GeoJSON>
 	</MapLibre>
 </div>
+
+{#if clickedFeature}
+	<p>バス停名: <span class="font-bold text-gray-800">{clickedFeature.name}</span></p>
+{/if}
