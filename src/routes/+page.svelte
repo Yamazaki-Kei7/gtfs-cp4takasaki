3fa500d816825583711d6f9447271d3864b8771a
yama-kei <yamazaki@mierune.co.jp>
2024-10-27 17:54:14 +0900

    Update page layout and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,31 @@
 	import LayerPanel from './LayerPanel.svelte';
 	import MapComponent from './MapComponent.svelte';
 	import DateTimeSlider from './DateTimeSlider.svelte';
+
+	let minutes: number = 0;
+	let dayType: string = '';
 </script>
 
-<body class="flex flex-col h-screen p-2">
+<body class="flex flex-col h-screen p-4 -m-2">
+	<!-- Header Section -->
 	<div class="flex-none">
 		<Header />
 	</div>
 
-	<div class="pt-2 border-2">
+	<!-- DateTimeSlider Section -->
+	<div class="flex-none pt-2">
 		<DateTimeSlider />
 	</div>
 
-	<div class="flex flex-grow pt-2 gap-2 h-full">
+	<!-- Main Content Section -->
+	<div class="flex flex-grow pt-2 gap-2 overflow-hidden">
+		<!-- Sidebar LayerPanel -->
 		<div class="w-2/12 h-full">
 			<LayerPanel />
 		</div>
 
-		<div class="w-10/12">
+		<!-- MapComponent -->
+		<div class="w-10/12 h-full overflow-hidden">
 			<MapComponent />
 		</div>
 	</div>
